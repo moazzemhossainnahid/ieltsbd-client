a1a1f35063528ca927b4759ede6514cf4cf18bcb
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-21 08:57:52 +0600

    add contact page

--- src/Pages/ContactUS/ContactUS.jsx
+++ src/Pages/ContactUS/ContactUS.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ContectBanner from '../../Components/Pages/ContactUS/ContectBanner';
+import ContactMap from '../../Components/Pages/ContactUS/ContactMap';
+import ContactApply from '../../Components/Pages/ContactUS/ContactApply';
 
 const ContactUS = () => {
     return (
-        <div>
-            
+        <div className='w-full'>
+            <ContectBanner />
+            <ContactMap />
+            <ContactApply />
         </div>
     );
 };
